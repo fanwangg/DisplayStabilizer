1f07eaea78acb875dff0b31505eb178971f9c248
nickisverygood <nickisverygood@hotmail.com.tw>
2015-11-15 12:44:39 +0800

    add DemoDraw

--- app/src/main/java/com/project/nicki/displaystabilizer/UI/DemoDrawUI.java
+++ app/src/main/java/com/project/nicki/displaystabilizer/UI/DemoDrawUI.java
@@ -24,7 +24,7 @@ import org.opencv.core.MatOfKeyPoint;
 
 public class DemoDrawUI extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
     DemoDraw DD;
-
+    //testgit
 
 /////from getFrontcam
 
